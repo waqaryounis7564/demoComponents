274b5236e71beef758624f2e5232e55978278ce1
Waqar <waqar.younas@2iqresearch.com>
2020-08-30 12:44:20 +0500

    custom directive with background properrty set to Dom element

--- src/app/class-directive.directive.ts
+++ src/app/class-directive.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appClassDirective]',
 })
 export class ClassDirectiveDirective {
-  constructor() {
+  constructor(private element: ElementRef) {
     console.log('hia');
+  }
+  @Input() set color(col: string) {
+    this.element.nativeElement.style.backgroundColor = col;
   }
 }
